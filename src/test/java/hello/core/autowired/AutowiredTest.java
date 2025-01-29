2bb838eb2e1b5537b86cab576ddc28c54d362682
kiki3123 <sekihwan@naver.com>
2025-01-29 21:21:23 +0900

    옵션처리

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,39 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+
+    }
+
+     static class TestBean {
+
+        @Autowired(required = false)        // 자동 주입할 대상이 없으면 출력 안됨.
+        public void setNoBean1(Member member) {
+            System.out.println("setNoBean1 = " + member);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("setNoBean2 = " + member);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("setNoBean3 = " + member);
+        }
+
+    }
+
+
+}
